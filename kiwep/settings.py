2b0c23d1a16a31655b73391400f45c373a5a191c
Lea Soussan <leampdi@gmail.com>
2021-03-25 15:19:28 +0200

    db set upwsgi and setting change

--- kiwep/settings.py
+++ kiwep/settings.py
@@ -110,16 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     # },
 ]
 
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
-
-
-
-
-
-
-
+DATABASES = { 'default' : dj_database_url.config()}
 
 
 
