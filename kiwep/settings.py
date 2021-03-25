d51140df0251eafb28bfac4b2cc48fc49ab7f266
Lea Soussan <leampdi@gmail.com>
2021-03-25 20:01:37 +0200

    translation Fr

--- kiwep/settings.py
+++ kiwep/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     # },
 ]
 
-DATABASES = { 'default' : dj_database_url.config()}
+# DATABASES = { 'default' : dj_database_url.config()}
 
 
 
@@ -151,9 +151,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
