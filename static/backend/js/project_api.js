14f9a737ef2045192d7894e431fc6e864e21d6b4
irinaignatenok <specterhauz@gmail.com>
2021-05-18 11:18:16 +0300

    api

--- static/backend/js/project_api.js
+++ static/backend/js/project_api.js
@@ -11,22 +11,69 @@ console.log('Is this page working?')
 //   }
 // }
 // catchAPI()
-
 const fetchData = (data) => {
-    // fetch('https://jsonplaceholder.typicode.com/users')
-    fetch('http://127.0.0.1:8000/project-list/')
+    fetch('/api_project/project_list/')
+// fetch('/api_project/project_update/{id}/')
         .then(res => res.json())
-        .then(data => displayProducts(data))
+        .then(data =>
+          displayProducts(data))
 }
 
-const displayProducts = (list) => {
-	const root = document.getElementById('getApi');
-	root.innerHTML ='';
-	for(i in list){
-		let text = document.createElement('h2');
-		text.innerHTML = list[i].name
 
-		root.appendChild(text)
+const displayProducts = (list) => {
+
+  const diva = document.getElementById('getData');
+  console.log(diva)
+	const table = document.createElement("table");
+  table.className = "table table-striped"
+  let row = document.createElement("tr");
+  let table_head = document.createElement("thead")
+
+
+
+  diva.appendChild(table);
+  table.appendChild(table_head);
+  table_head.appendChild(row);
+  let thead = document.createElement("th");
+  let thead1 = document.createElement("th");
+  let thead2 = document.createElement("th");
+
+  row.appendChild(thead)
+  row.appendChild(thead1)
+  row.appendChild(thead2)
+  let text = document.createTextNode(' project name ')
+  let text1 = document.createTextNode(' speaker name ')
+  let text2 = document.createTextNode(' time to complete ')
+  thead.appendChild(text)
+  thead1.appendChild(text1)
+  thead2.appendChild(text2)
+
+  let tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+
+	for(i in list){
+
+    let row1 = document.createElement("tr")
+	   let tableData = document.createElement("td")
+     let tableData1 = document.createElement("td")
+     let tableData2 = document.createElement("td")
+     let link = document.createElement("a")
+     let text_link = document.createTextNode(list[i].name)
+     let text_link1 = document.createTextNode(list[i].speaker)
+     let text_link2 = document.createTextNode(list[i].time_to_complet)
+
+     tbody.appendChild(row1)
+     row1.appendChild(tableData)
+     row1.appendChild(tableData1)
+     row1.appendChild(tableData2)
+     tableData.appendChild(link)
+     link.appendChild(text_link)
+     tableData1.appendChild(text_link1)
+     tableData2.appendChild(text_link2)
+     let url = '/project-detail/' + list[i].id
+     // url.searchParams.set('id', )
+     link.setAttribute("href", url )
+     link.setAttribute("style", "color:black;")
 	}
 }
   fetchData()
@@ -34,16 +81,34 @@ const displayProducts = (list) => {
 
 
 
+const fetchCreateData = (data) => {
+    fetch('/api_project/project_create/',
+      {
+          method: 'POST',
+          headers: {
+              'Content-type': 'application/json'
+          },
+          body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+        })
+}
 
 
 
-
-//   .then(res =>
-//     if(res.ok) {
-//       console.log('SUCCESS')
-//     } else {
-//       console.log("Not successful")
-//     }
-//     res.json())
-//   .then(data => console.log(data))
-//   .catch(err => console.log('ERROR'))
+// const displayProjects = (list) =>{
+//   const root = document.getElementById('getData');
+//   root.innerHTML ='';
+// 	for(i in list){
+// 		let text = document.createElement('h2');
+// 		text.innerHTML = list[i].acquried_skills
+//     let text1 = document.createElement('input');
+// 		text.innerHTML = list[i].speaker
+//
+// 		root.appendChild(text)
+//     root.appendChild(text1)
+// 	}
+// }
+// fetchCreateData()
---
+++ static/backend/js/project_api_details.js
@@ -0,0 +1,14 @@
+const fetchProjectDetails = (data) => {
+  fetch('/api_project/project_detail/')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+  })
+    // displayProjectDetails())
+}
+
+
+const displayProjectDetails = (list) => {
+  const div = document.getElementById("project_detail")
+  console.log(div)
+}
