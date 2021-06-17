4dddba1caad09c781820f34cc341550b9e40d731
Lea Soussan <leampdi@gmail.com>
2021-06-17 18:24:15 +0300

    OK comment

--- message/urls.py
+++ message/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import (
     # my_inbox,
     DiscussionCreateView,
-    DiscussionView,
     CommentCreateView,
 
 )
@@ -11,6 +10,5 @@ from .views import (
 urlpatterns = [
     # path('my_inbox', my_inbox, name= 'my_inbox'),
     path('discussion-add', DiscussionCreateView.as_view(), name= 'discussion_add'),
-    path('discussion-view/<int:pk>', DiscussionView.as_view(), name= 'discussion_view'),
     path('comment-add/',CommentCreateView.as_view(), name = 'comment_add')
 ]
