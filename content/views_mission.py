a52e770c2b55d6bf552838b591cd5fb972ed7217
lea <leampdi@gmail.com>
2021-11-17 11:21:55 +0200

    student ux fixed- not speaker ux

--- content/views_mission.py
+++ content/views_mission.py
@@ -149,9 +149,12 @@ class CollectiveMissionDetailView(ProfileCheckPassesTestMixin, DetailView):
         context['status_form'] = MissionSpeakerStatusAnswerForm()
         if self.request.user.is_student:
             context = super().get_context_data(**kwargs)
-            # what if we dont have >>.there is an error
-            # context['individual_collective_mission'] = IndividualCollectiveMission.objects.get(parent_mission=self.object.id,
+            # TODO to call the collective mission
+            # if self.object.individualcollectivemission_set:
+            #     context['individual_collective_mission'] = IndividualCollectiveMission.objects.get(parent_mission=self.object.id,
             #     attributed_to=self.request.user.profile())
+            # else:
+            #     print('no_ind_m')
             return context
         # if self.object.attributed_to.exists() and self.request.user in self.object.attributed_to.all():
         #     context['collective_individual'] = IndividualCollectiveMission.objects.filter(parent_mission__id=get_object_or_404())
--- message/urls.py
+++ message/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     # my_inbox,
     DiscussionCreateView,
-
+    AnswerDeletView,
     CommentCreateView,
     AnswerCreateView,
     SpeakerAnswerMissionStatusView,
@@ -17,6 +17,7 @@ urlpatterns = [
     path('comment-add/',CommentCreateView.as_view(), name = 'comment_add'),
     path('answer-add/', AnswerCreateView.as_view(), name='create_answer'),
     path('answer-edit/<int:pk>', AnswerUpdateView.as_view(), name='update_answer'),
+    path('delete-edit/<int:pk>', AnswerDeletView.as_view(), name='delete_answer'),
     path('answer-edit-status/<int:pk>', SpeakerAnswerMissionStatusView.as_view(), name='answer_edit_status'),
     path('answer-grade/<int:pk>', SpeakerGradeAnswerView.as_view(), name='answer_grade_add'),
 
